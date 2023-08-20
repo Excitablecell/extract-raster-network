52af59d4b49bb5b3a3facb0ff3e0d55dfc317a85
SS47816 <ss1057532022@gmail.com>
2023-08-21 04:29:19 +0800

    feat: add cubic spline 2d fit

---
+++ cubic_spline_fit.py
@@ -0,0 +1,186 @@
+import math
+import numpy as np
+
+from scipy.optimize import curve_fit
+
+def cubic_func(x, a, b, c, d):
+    return a * x**3 + b * x**2 + c * x + d
+
+class CubicSpline1D:
+    """
+    1D Cubic Spline class
+
+    Parameters
+    ----------
+    x : list
+        x coordinates for data points. This x coordinates must be
+        sorted
+        in ascending order.
+    y : list
+        y coordinates for data points
+    """
+
+    def __init__(self, x, y):
+
+        h = np.diff(x)
+        if np.any(h < 0):
+            raise ValueError("x coordinates must be sorted in ascending order")
+
+        self.x = x
+        self.y = y
+        self.nx = len(x)  # dimension of x
+
+        # calc coefficients
+        popt, pcov = curve_fit(cubic_func, x, y, check_finite=True)
+        self.a, self.b, self.c, self.d = popt
+        self.score = np.linalg.cond(pcov)
+
+    def calc_position(self, x):
+        """
+        Calc `y` position for given `x`.
+
+        if `x` is outside the data point's `x` range, return None.
+
+        Returns
+        -------
+        y : float
+            y position for given x.
+        """
+        if x < self.x[0]:
+            return None
+        elif x > self.x[-1]:
+            return None
+
+        position = cubic_func(x, self.a, self.b, self.c, self.d)
+
+        return position
+
+    def calc_first_derivative(self, x):
+        """
+        Calc first derivative at given x.
+
+        if x is outside the input x, return None
+
+        Returns
+        -------
+        dy : float
+            first derivative for given x.
+        """
+
+        if x < self.x[0]:
+            return None
+        elif x > self.x[-1]:
+            return None
+
+        dy = 3 * self.a * x**2 + 2 * self.b * x + self.c
+        return dy
+
+    def calc_second_derivative(self, x):
+        """
+        Calc second derivative at given x.
+
+        if x is outside the input x, return None
+
+        Returns
+        -------
+        ddy : float
+            second derivative for given x.
+        """
+
+        if x < self.x[0]:
+            return None
+        elif x > self.x[-1]:
+            return None
+
+        ddy = 6 * self.a * x + 2 * self.b
+        return ddy
+
+
+class CubicSpline2D:
+    """
+    Cubic CubicSpline2D class
+
+    Parameters
+    ----------
+    x : list
+        x coordinates for data points.
+    y : list
+        y coordinates for data points.
+    """
+
+    def __init__(self, x, y):
+        self.s = self.__calc_s(x, y)
+        self.sx = CubicSpline1D(self.s, x)
+        self.sy = CubicSpline1D(self.s, y)
+
+    def __calc_s(self, x, y):
+        dx = np.diff(x)
+        dy = np.diff(y)
+        self.ds = np.hypot(dx, dy)
+        s = [0]
+        s.extend(np.cumsum(self.ds))
+        return s
+
+    def calc_position(self, s):
+        """
+        calc position
+
+        Parameters
+        ----------
+        s : float
+            distance from the start point. if `s` is outside the data point's
+            range, return None.
+
+        Returns
+        -------
+        x : float
+            x position for given s.
+        y : float
+            y position for given s.
+        """
+        x = self.sx.calc_position(s)
+        y = self.sy.calc_position(s)
+
+        return x, y
+
+    def calc_curvature(self, s):
+        """
+        calc curvature
+
+        Parameters
+        ----------
+        s : float
+            distance from the start point. if `s` is outside the data point's
+            range, return None.
+
+        Returns
+        -------
+        k : float
+            curvature for given s.
+        """
+        dx = self.sx.calc_first_derivative(s)
+        ddx = self.sx.calc_second_derivative(s)
+        dy = self.sy.calc_first_derivative(s)
+        ddy = self.sy.calc_second_derivative(s)
+        k = (ddy * dx - ddx * dy) / ((dx ** 2 + dy ** 2)**(3 / 2))
+        return k
+
+    def calc_yaw(self, s):
+        """
+        calc yaw
+
+        Parameters
+        ----------
+        s : float
+            distance from the start point. if `s` is outside the data point's
+            range, return None.
+
+        Returns
+        -------
+        yaw : float
+            yaw angle (tangent vector) for given s.
+        """
+        dx = self.sx.calc_first_derivative(s)
+        dy = self.sy.calc_first_derivative(s)
+        yaw = math.atan2(dy, dx)
+        return yaw
